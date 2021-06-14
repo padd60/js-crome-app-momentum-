10ff65bc0bf035253005e98c2a24d71cc33975f5
junghwankim <padd60@naver.com>
2021-06-14 22:50:10 +0900

    const and let

---
+++ app.js
@@ -0,0 +1,12 @@
+const a = 5;
+const b = 2;
+let myName = "junghwan";
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello " + myName);
+
+myName = "jhkim";
+
+console.log("your new name is " + myName);
