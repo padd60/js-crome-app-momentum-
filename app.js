5cf9e0b7e7538cdeb4f0539b4df4c7f436420d52
junghwankim <padd60@naver.com>
2021-06-15 21:13:09 +0900

    function part2 and object + function

--- app.js
+++ app.js
@@ -5,3 +5,26 @@ function sayHello(nameOfPerson, age) {
 sayHello("junghwan", 27);
 sayHello("kim", 25);
 sayHello("john", 30);
+
+function plus(a, b) {
+  console.log(a + b);
+}
+
+plus(8, 60);
+
+function divide(a, b) {
+  console.log(a / b);
+}
+
+divide(98, 20);
+
+const player = {
+  name: "kim",
+  sayBye: function (personName) {
+    console.log("Bye! " + personName);
+  },
+};
+
+console.log(player.name);
+player.sayBye("junghwan");
+player.sayBye("john");
