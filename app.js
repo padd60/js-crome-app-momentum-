3330a113ab825665fcc5851268e891d6d719d71b
junghwankim <padd60@naver.com>
2021-06-16 19:00:31 +0900

    condisionals1

--- app.js
+++ app.js
@@ -1,20 +1,3 @@
-const calculator = {
-  add: function (a, b) {
-    console.log("hello");
-    return a + b;
-  },
-  minus: function (a, b) {
-    return a - b;
-  },
-  multiply: function (a, b) {
-    return a * b;
-  },
-  divide: function (a, b) {
-    return a / b;
-  },
-  squared: function (a, b) {
-    return a ** b;
-  },
-};
+const age = parseInt(prompt("how old are you?"));
 
-const addresult = calculator.add(2, 3);
+console.log(age);
