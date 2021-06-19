5b73f8fdfd586535a0ce063abe5baf331b272f78
junghwankim <b6nqctxwfs@privaterelay.appleid.com>
2021-06-19 23:22:43 +0900

    html in javascript

--- app.js
+++ app.js
@@ -1,23 +1,6 @@
-const age = parseInt(prompt("how old are you?"));
+const title = document.getElementById("title");
 
-if (isNaN(age) || age < 0) {
-  console.log("please write positive number");
-} else if (age < 18) {
-  console.log("you are too young");
-} else if (age >= 18 && age <= 50) {
-  console.log("you can drink!");
-} else if (age > 50 && age <= 80) {
-  console.log("you should exercise!");
-} else {
-  console.log("you can't drink!");
-}
+title.innerText = "got you";
 
-true || true === true;
-true || false === true;
-false || true === true;
-false || false === false;
-
-true && true === true;
-true && false === false;
-false && true === false;
-false && false === false;
+console.log(title.id);
+console.log(title.className);
