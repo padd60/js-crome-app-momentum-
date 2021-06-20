d648e807cd6df2636040585d3321c35216f71184
junghwankim <b6nqctxwfs@privaterelay.appleid.com>
2021-06-20 23:56:03 +0900

    login event

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const loginForm = document.querySelector(".login-form");
 const loginInput = document.querySelector(".login-form input");
 
-function onLoginSubmit(tomato) {
-  tomato.preventDefault();
-  const username = loginInput.value;
-  console.log(username);
+function onLoginSubmit(event) {
+  event.preventDefault();
+  console.log(loginInput.value);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
