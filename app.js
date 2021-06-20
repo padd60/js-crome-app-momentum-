ad4a4338041d6c0b7f5e3890ad18a1c9df6ce419
junghwankim <b6nqctxwfs@privaterelay.appleid.com>
2021-06-20 21:42:24 +0900

    css in jsp last

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
-  const currentColor = h1.style.color;
-  let newColor;
-  if (currentColor === "blue") {
-    newColor = "tomato";
-  } else {
-    newColor = "blue";
-  }
-  h1.style.color = newColor;
+  h1.classList.toggle("active");
 }
+
 h1.addEventListener("click", handleTitleClick);
+
+// const clickedClass = "active";
+// if (h1.classList.contains(clickedClass)) {
+//   h1.classList.remove(clickedClass);
+// } else {
+//   h1.classList.add(clickedClass);
+// }
