c1c85d2407092885c38c6ea362279e5f6907bbb8
junghwankim <padd60@naver.com>
2021-06-27 23:47:51 +0900

    background img & randomness

---
+++ js/background.js
@@ -0,0 +1,9 @@
+const images = ["nature.jpg", "ocean.jpg", "sky.jpg"];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement("img");
+
+bgImage.src = `img/${chosenImage}`;
+
+document.body.appendChild(bgImage);
